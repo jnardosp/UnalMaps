beac05fa02b3235a6f7e6e275f97191c8cfbe123
Nardosp <jsotop@unal.edu.co>
2022-10-17 17:27:39 -0500

    Revert "Commit 9 10/10 version, with mapbox, and viewPager1."
    This reverts commit f01a351de84a83a33f249c040c08f0d45b0f5229.

--- app/src/main/java/com/poo/unalmaps/MainActivity.java
+++ app/src/main/java/com/poo/unalmaps/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.locateButton;
+        FloatingActionButton fab = binding.fab;
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
