d755def8f098d68a62db289b62f408bbbfbd21f3
Nardosp <jsotop@unal.edu.co>
2022-10-19 20:04:02 -0500

    Commit 12.2 19/10 version, clases Place and Point. autor: jsotop

---
+++ app/src/main/java/com/poo/unalmaps/Place.java
@@ -0,0 +1,62 @@
+package com.poo.unalmaps;
+
+import java.util.List;
+
+//Made by N0chez
+public class Place {
+    private final String name;
+    private final List nickNames;
+    private final String description;
+    private final Point placePoint;
+    private final List placeEntries;
+    private final String buildingNumber;
+    private final String faculty;
+    private boolean approved = false;
+
+    public Place (String name, List nickNames, String description, Point placePoint, List placeEntries,
+                  String buildingNumber, String faculty) {
+        this.name = name;
+        this.nickNames = nickNames;
+        this.description = description;
+        this.placePoint = placePoint;
+        this.placeEntries = placeEntries;
+        this.buildingNumber = buildingNumber;
+        this.faculty = faculty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List getNickNames() {
+        return nickNames;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Point getPlacePoint() {
+        return placePoint;
+    }
+
+    public List getPlaceEntries() {
+        return placeEntries;
+    }
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    private void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+}
---
+++ app/src/main/java/com/poo/unalmaps/Point.java
@@ -0,0 +1,28 @@
+package com.poo.unalmaps;
+
+//Made by N0chez
+public class Point {
+    private float cordX;
+    private float cordY;
+
+    public Point (float x, float y) {
+        this.cordX = x;
+        this.cordY = y;
+    }
+
+    public Float getCordX() {
+        return cordX;
+    }
+
+    public Float getCordY() {
+        return cordY;
+    }
+
+    public void setCordX(Float cordX) {
+        this.cordX = cordX;
+    }
+
+    public void setCordY(Float cordY) {
+        this.cordY = cordY;
+    }
+}
